0c73d4c29a710c988d8a7f8cfdc9c31100a987b6
kuluruvineeth <kuluruvineeth8623@gmail.com>
2023-06-04 19:27:28 +0530

    index page is created

---
+++ data/section-templates.js
@@ -0,0 +1,419 @@
+export const sectionTemplates = [
+  {
+    slug: "title_and_description",
+    name: "Title and Description",
+    markdown: `
+# Project Title
+A brief description of what this project does and who it's for
+
+`,
+  },
+  {
+    slug: "installation",
+    name: "Installation",
+    markdown: `
+## Installation 
+Install my-project with npm
+\`\`\`bash 
+    npm install my-project
+    cd my-project
+\`\`\`
+        `,
+  },
+  {
+    slug: "logo",
+    name: "Logo",
+    markdown: `
+![Logo](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/th5xamgrr6se0x5ro4g6.png)
+
+    `,
+  },
+  {
+    slug: "run-locally",
+    name: "Run Locally",
+    markdown: `
+## Run Locally
+
+Clone the project
+
+\`\`\`bash
+  git clone https://link-to-project
+\`\`\`
+
+Go to the project directory
+
+\`\`\`bash
+  cd my-project
+\`\`\`
+
+Install dependencies
+
+\`\`\`bash
+  npm install
+\`\`\`
+
+Start the server
+
+\`\`\`bash
+  npm run start
+\`\`\`
+
+  `,
+  },
+  {
+    slug: "screenshots",
+    name: "Screenshots",
+    markdown: `
+## Screenshots
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+  `,
+  },
+  {
+    slug: "env-variables",
+    name: "Environment Variables",
+    markdown: `
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+\`API_KEY\`
+
+\`ANOTHER_API_KEY\`
+
+  `,
+  },
+  {
+    slug: "features",
+    name: "Features",
+    markdown: `
+## Features
+
+- Light/dark mode toggle
+- Live previews
+- Fullscreen mode
+- Cross platform
+
+  `,
+  },
+  {
+    slug: "usage-examples",
+    name: "Usage/Examples",
+    markdown: `
+## Usage/Examples
+
+\`\`\`javascript
+import Component from 'my-project'
+
+function App() {
+  return <Component />
+}
+\`\`\`
+
+  `,
+  },
+  {
+    slug: "api",
+    name: "API Reference",
+    markdown: `
+## API Reference
+
+#### Get all items
+
+\`\`\`http
+  GET /api/items
+\`\`\`
+
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| \`api_key\` | \`string\` | **Required**. Your API key |
+
+#### Get item
+
+\`\`\`http
+  GET /api/items/$\{id}
+\`\`\`
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| \`id\`      | \`string\` | **Required**. Id of item to fetch |
+
+#### add(num1, num2)
+
+Takes two numbers and returns the sum.
+
+  `,
+  },
+  {
+    slug: "contributing",
+    name: "Contributing",
+    markdown: `
+## Contributing
+
+Contributions are always welcome!
+
+See \`contributing.md\` for ways to get started.
+
+Please adhere to this project's \`code of conduct\`.
+
+  `,
+  },
+  {
+    slug: "tests",
+    name: "Running Tests",
+    markdown: `
+## Running Tests
+
+To run tests, run the following command
+
+\`\`\`bash
+  npm run test
+\`\`\`
+
+  `,
+  },
+  {
+    slug: "license",
+    name: "License",
+    markdown: `
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+  `,
+  },
+  {
+    slug: "badges",
+    name: "Badges",
+    markdown: `
+## Badges
+
+Add badges from somewhere like: [shields.io](https://shields.io/)
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
+
+  `,
+  },
+  {
+    slug: "roadmap",
+    name: "Roadmap",
+    markdown: `
+## Roadmap
+
+- Additional browser support
+
+- Add more integrations
+
+  `,
+  },
+  {
+    slug: "authors",
+    name: "Authors and Acknowledgement",
+    markdown: `
+## Authors and Acknowledgement
+
+- [@kuluruvineeth](https://www.github.com/kuluruvineeth) for development and design.
+
+  `,
+  },
+  {
+    slug: "support",
+    name: "Support",
+    markdown: `
+## Support
+
+For support, email fake@fake.com or join our Slack channel.
+
+  `,
+  },
+  {
+    slug: "feedback",
+    name: "Feedback",
+    markdown: `
+## Feedback
+If you have any feedback, please reach out to us at fake@fake.com
+
+  `,
+  },
+  {
+    slug: "related",
+    name: "Related",
+    markdown: `
+## Related
+
+Here are some related projects
+
+[Awesome README](https://github.com/matiassingers/awesome-readme)
+
+  `,
+  },
+  {
+    slug: "demo",
+    name: "Demo",
+    markdown: `
+## Demo
+
+Insert gif or link to demo
+
+  `,
+  },
+  {
+    slug: "tech",
+    name: "Tech",
+    markdown: `
+## Tech Stack
+
+**Client:** React, Redux, TailwindCSS
+
+**Server:** Node, Express
+
+  `,
+  },
+  {
+    slug: "optimizations",
+    name: "Optimizations",
+    markdown: `
+## Optimizations
+
+What optimizations did you make in your code? E.g. refactors, performance improvements, accessibility
+
+  `,
+  },
+  {
+    slug: "lessons",
+    name: "Lessons",
+    markdown: `
+## Lessons Learned
+
+What did you learn while building this project? What challenges did you face and how did you overcome them?
+
+  `,
+  },
+  {
+    slug: "faq",
+    name: "FAQ",
+    markdown: `
+## FAQ
+
+#### Question 1
+
+Answer 1
+
+#### Question 2
+
+Answer 2
+
+  `,
+  },
+  {
+    slug: "used-by",
+    name: "Used By",
+    markdown: `
+## Used By
+
+This project is used by the following companies:
+
+- Company 1
+- Company 2
+
+  `,
+  },
+  {
+    slug: "documentation",
+    name: "Documentation",
+    markdown: `
+## Documentation
+
+[Documentation](https://linktodocumentation)
+
+  `,
+  },
+  {
+    slug: "deployment",
+    name: "Deployment",
+    markdown: `
+## Deployment
+
+To deploy this project run
+
+\`\`\`bash
+  npm run deploy
+\`\`\`
+
+  `,
+  },
+  {
+    slug: "appendix",
+    name: "Appendix",
+    markdown: `
+## Appendix
+
+Any additional information goes here
+
+  `,
+  },
+  {
+    slug: "colorreference",
+    name: "Color Reference",
+    markdown: `## Color Reference
+| Color             | Hex                                                                |
+| ----------------- | ------------------------------------------------------------------ |
+| Example Color | ![#0a192f](https://via.placeholder.com/10/0a192f?text=+) #0a192f |
+| Example Color | ![#f8f8f8](https://via.placeholder.com/10/f8f8f8?text=+) #f8f8f8 |
+| Example Color | ![#00b48a](https://via.placeholder.com/10/00b48a?text=+) #00b48a |
+| Example Color | ![#00d1a0](https://via.placeholder.com/10/00b48a?text=+) #00d1a0 | 
+
+`,
+  },
+  {
+    slug: "github-profile-intro",
+    name: "Github Profile - Introduction",
+    markdown: `
+# Hi, I'm Kuluru Vineeth! 👋
+  `,
+  },
+  {
+    slug: "github-profile-about-me",
+    name: "Github Profile - About Me",
+    markdown: `
+## 🚀 About Me
+I'm a full stack developer...
+  `,
+  },
+  {
+    slug: "github-profile-skills",
+    name: "Github Profile - Skills",
+    markdown: `
+## 🛠 Skills
+Javascript, HTML, CSS...
+  `,
+  },
+  {
+    slug: "github-profile-links",
+    name: "Github Profile - Links",
+    markdown: `
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://www.kuluruvineeth.com/)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/kuluruvineeth)
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/kuluruvineeth)
+  `,
+  },
+  {
+    slug: "github-profile-other",
+    name: "Github Profile - Other",
+    markdown: `
+## Other Common Github Profile Sections
+👩‍💻 I'm currently working on...
+🧠 I'm currently learning...
+👯‍♀️ I'm looking to collaborate on...
+🤔 I'm looking for help with...
+💬 Ask me about...
+📫 How to reach me...
+😄 Pronouns...
+⚡️ Fun fact...
+  `,
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,129 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <div className="bg-gray-50">
+        <div className="relative overflow-hidden">
+          <div className="absolute inset-y-0 h-full w-full" aria-label="true">
+            <div className="relative h-full">
+              <svg
+                className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 sm:-translate-x-1/2 md:-translate-y-1/2 lg:-translate-x-3/4"
+                width={404}
+                height={784}
+                fill="none"
+                viewBox="0 0 404 784"
+              >
+                <defs>
+                  <pattern
+                    id="d2a68204-c383-44b1-b99f-42ccff4e5365"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={404}
+                  height={784}
+                  fill="url(#d2a68204-c383-44b1-b99f-42ccff4e5365)"
+                />
+              </svg>
+            </div>
+          </div>
+          <div className="relative pt-6 pb-16 sm:pb-24">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6">
+              <nav className="relative flex items-center justify-between sm:h-10 md:justify-center">
+                <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
+                  <div className="flex items-center justify-between w-full md:w-auto">
+                    <Link href="/">
+                      <div className="flex items-center">
+                        <img
+                          className="h-8 mt-1 w-auto cursor-pointer"
+                          src="readme.svg"
+                        />
+                        <p className="text-gray-800 ml-2 -mb-2 text-md">
+                          readmeCreator
+                        </p>
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+                <div className="relative w-28 items-center flex-1 md:absolute md:inset-y-0 md:right-0 z-10">
+                  Language
+                </div>
+              </nav>
+            </div>
+
+            <div className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
+              <div className="text-center">
+                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                  <span className="block">The fastest way to create a</span>
+                  <span className="block text-green-600">README</span>
+                </h1>
+                <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                  Our simple editor allows you to quickly add and customize all
+                  the sections you need for your project's readme
+                </p>
+              </div>
+              <div className="flex justify-center mt-6">
+                <span className="inline-flex rounded-md shadow">
+                  <Link
+                    href="/editor"
+                    className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-lg text-green-600 bg-white hover:text-green-500"
+                  >
+                    Get Started
+                  </Link>
+                </span>
+              </div>
+              <div className="text-center">
+                <p className="mt-3 mx-auto text-sm text-gray-900">100% free</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative">
+          <div className="absolute inset-0 flex flex-col">
+            <div className="flex-1" />
+            <div className="flex-1 w-full bg-gray-800" />
+          </div>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6">
+            <img
+              className="relative rounded-md shadow-lg"
+              src="/readmeCreator.png"
+              alt="App Screenshot"
             />
-          </a>
+          </div>
+        </div>
+        <div className="bg-gray-800">
+          <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+            <h2 className="text-center text-gray-400 text-sm font-semibold tracking-wide">
+              Made with love &#9825; by{" "}
+              <a
+                className="hover:text-green-500"
+                href="https://www.kuluruvineeth.com"
+                target="__blank"
+              >
+                Kuluru Vineeth
+              </a>
+            </h2>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
